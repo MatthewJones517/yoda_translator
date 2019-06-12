b666940a18756dd863ddbe4d293c59fc09859111
Matthew Jones <Mail@MatthewRJones.me>
2019-06-12 19:53:48 -0400

    Took autogenerated files down to bare bones.

---
+++ lib/src/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  Widget build(context) {
+    // Main widget that provides basic functions needed for the app to run.
+    return MaterialApp(
+      title: 'TCG Counter',
+      debugShowCheckedModeBanner: false,
+      home: Text('Hello From a Galaxy Far, Far Away!!!'),
+    );
+  }
+}
