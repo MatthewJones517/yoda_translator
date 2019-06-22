97ea9c066f2e75350e65434aa4ea3e4b3c84d74c
Matthew Jones <Mail@MatthewRJones.me>
2019-06-22 17:37:30 -0400

    Created input text box.

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -17,6 +17,53 @@ class Home extends StatelessWidget {
           fit: BoxFit.cover,
         ),
       ),
+      child: uiItems(),
+    );
+  }
+
+  Widget uiItems() {
+    return Container(
+      margin: EdgeInsets.all(25),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          inputTextBox(),
+        ],
+      ),
+    );
+  }
+
+  Widget inputTextBox() {
+    return Expanded(
+      flex: 6,
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            width: 3,
+            color: Color(0xff00d2ff),
+          ),
+        ),
+        padding: EdgeInsets.all(15),
+        child: inputTextField(),
+      ),
+    );
+  }
+
+  Widget inputTextField() {
+    return TextField(
+      decoration: InputDecoration(
+        border: InputBorder.none,
+        hintText: 'Enter text to translate...',
+        hintStyle: TextStyle(
+          color: Color(0xff00d2ff),
+        ),
+      ),
+      keyboardType: TextInputType.multiline,
+      maxLines: null,
+      style: TextStyle(
+        fontSize: 28,
+        color: Color(0xff00d2ff),
+      ),
     );
   }
 }
