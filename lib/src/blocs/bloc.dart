80bd109c7583c44a8c2fd86a1412e245c435ba52
Matthew Jones <Mail@MatthewRJones.me>
2019-11-08 09:37:53 -0500

    Set up user input stream in bloc and added translate method.

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -1,3 +1,18 @@
+import 'package:rxdart/rxdart.dart';
+
 class Bloc {
-  
+  // Set up stream for translation text
+  final BehaviorSubject<String> _userInput = BehaviorSubject<String>();
+
+  // Set up stream getters
+  Observable<String> get userInput => _userInput.stream;
+  Function(String) get userInputSink => _userInput.sink.add;
+
+  void translate() {
+    print("Text to Translate: ${_userInput.value}");
+  }
+
+  dispose() {
+    _userInput.close();
+  }
 }
