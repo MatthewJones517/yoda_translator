931b77ecdb8dd8a36d66b7beff1be7032b907a6b
Matthew Jones <mail@matthewrjones.me>
2020-10-18 20:27:52 -0400

    Connected Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:yoda_translator/src/app.dart';
+import 'package:provider/provider.dart';
+import 'package:yoda_translator/src/services/translator_service.dart';
+import 'src/app.dart';
 
 void main() async {
-  runApp(App());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => TranslatorService(),
+      child: App(),
+    ),
+  );
 }
